8f53136096c25e40e58d5507f78cb478798c19d3
santh <santh@DESKTOP-JV5ATAC>
2017-08-03 15:14:36 -0500

    New Weather page

---
+++ MyWebApp/src/com/dmi/weather/Weatherinfo.java
@@ -0,0 +1,9 @@
+package com.dmi.weather;
+
+public class Weatherinfo {
+
+	public String getWeatherInfo(String cityName, String zipCode)
+	{
+		return zipCode;
+	}
+}
---
+++ MyWebApp/src/com/servlet/weatherinfo/Weather.java
@@ -0,0 +1,41 @@
+package com.servlet.weatherinfo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Weather
+ */
+@WebServlet("/Weather")
+public class Weather extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Weather() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String cityName = request.getParameter("city_name");
+		String zipcode = request.getParameter("zipcode");
+	}
+
+}
